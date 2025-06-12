913202cd00ff3c2e41053cd81d318cd42b6a4143
ChuSebastian <sebastian.chu@utec.edu.pe>
2025-06-11 21:15:12 -0500

    crear-bucket.py agregado

---
+++ crear_bucket.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    """Lambda para crear un bucket S3. El nombre del bucket se recibe vía evento JSON."""
+    s3_client = boto3.client('s3')
+
+    # Obtener el nombre del bucket desde el evento
+    bucket_name = event.get('nuevo-bucket')
+    
+    if not bucket_name:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Error: El nombre del bucket no fue proporcionado.')
+        }
+
+    try:
+        s3_client.create_bucket(Bucket=bucket_name)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f"Bucket '{bucket_name}' creado exitosamente.")
+        }
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error al crear el bucket: {e.response['Error']['Message']}")
+        }
+
